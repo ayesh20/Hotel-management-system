fe0e8ab97624aeb6c0776ad3571b83758a003aa3
Tharinda Pathirana <tharindapathirana27@gmail.com>
2026-01-25 11:32:57 +0530

    setup payment file

---
+++ backend/PaymentService/src/main/java/com/nsbm/group_04/Payment/PaymentApplication.java
@@ -0,0 +1,13 @@
+package com.nsbm.group_04.Payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentApplication.class, args);
+	}
+
+}
