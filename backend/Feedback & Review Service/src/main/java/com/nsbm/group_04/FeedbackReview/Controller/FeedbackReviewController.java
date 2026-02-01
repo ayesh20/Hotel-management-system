a9c8a7a2ae74e746b985c4e4090c941ff7968f86
Vila1227 <vilanya.udukumbura@gmail.com>
2026-02-02 00:09:09 +0530

    Created Feedback and Review module
    Created Feedback and Review module with packages(repository,entity,services,controller)

---
+++ backend/Feedback & Review Service/src/main/java/com/nsbm/group_04/FeedbackReview/Controller/FeedbackReviewController.java
@@ -0,0 +1,4 @@
+package com.nsbm.group_04.FeedbackReview.Controller;
+
+public class FeedbackReviewController {
+}
---
+++ backend/Feedback & Review Service/src/main/java/com/nsbm/group_04/FeedbackReview/services/impl/FeedbackReviewServiceImpl.java
@@ -0,0 +1,4 @@
+package com.nsbm.group_04.FeedbackReview.services.impl;
+
+public class FeedbackReviewServiceImpl {
+}
