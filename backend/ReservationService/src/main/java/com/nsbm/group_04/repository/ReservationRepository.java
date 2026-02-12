217c6993ddd3063f61e3fc342d59e1d3ac427a2d
SumindaBandara <sumindabandarapc@gmail.com>
2026-02-12 23:06:05 +0530

    reservation repository

--- backend/ReservationService/src/main/java/com/nsbm/group_04/repository/ReservationRepository.java
+++ backend/ReservationService/src/main/java/com/nsbm/group_04/repository/ReservationRepository.java
@@ -1,4 +1,11 @@
 package com.nsbm.group_04.repository;
 
-public interface ReservationRepository {
+import com.nsbm.group_04.entity.Reservation;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReservationRepository extends MongoRepository<Reservation, String> {
+    // That's it! Spring implements this automatically at runtime.
+    // If you need custom queries later, you can add them here.
 }
