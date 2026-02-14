f0727523b82022e102fa413fa6b0edbfe7987191
ruvindya28 <ruvindyasachini2000@gmail.com>
2026-02-14 12:30:36 +0530

    Create folder Structure and Connect MongoDB

---
+++ backend/BookingTransportService/src/main/java/com/nsbm/group_04/BookingTransportService/entity/Transport.java
@@ -0,0 +1,4 @@
+package com.nsbm.group_04.BookingTransportService.entity;
+
+public class Transport {
+}
---
+++ backend/BookingTransportService/src/main/java/com/nsbm/group_04/BookingTransportService/repository/TransportRepository.java
@@ -0,0 +1,4 @@
+package com.nsbm.group_04.BookingTransportService.repository;
+
+public interface TransportRepository {
+}
