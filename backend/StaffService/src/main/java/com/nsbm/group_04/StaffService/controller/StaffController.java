e36f0e35ca9fbb778513eaf4eee812f9b44872a9
Yasintha8 <yasinthachamikara28@gmail.com>
2026-02-16 23:51:30 +0530

    feat: add Read by id and update Staff api endpoint

--- backend/StaffService/src/main/java/com/nsbm/group_04/StaffService/controller/StaffController.java
+++ backend/StaffService/src/main/java/com/nsbm/group_04/StaffService/controller/StaffController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/staff")
@@ -27,4 +28,17 @@ public class StaffController {
     public List<Staff> getAllStaff() {
         return staffService.getAllStaff();
     }
+
+    //READ staff by ID
+    @GetMapping("/{id}")
+    public Optional<Staff> getStaffById(@PathVariable String id) {
+        return staffService.getStaffById(id);
+    }
+
+    // UPDATE staff
+    @PutMapping("/{id}")
+    public Staff updateStaff(@PathVariable String id, @RequestBody Staff staff) {
+        return staffService.updateStaff(id,staff);
+    }
+
 }
--- backend/StaffService/src/main/java/com/nsbm/group_04/StaffService/service/StaffService.java
+++ backend/StaffService/src/main/java/com/nsbm/group_04/StaffService/service/StaffService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StaffService {
@@ -20,6 +21,28 @@ public class StaffService {
     // Get all staff
     public List<Staff> getAllStaff(){
         return staffRepository.findAll();
+    }
+
+    // Get staff by ID
+    public Optional<Staff> getStaffById(String id) {
+        return staffRepository.findById(id);
+    }
+
+    // update staff
+    public Staff updateStaff(String id,Staff staff) {
+        Optional<Staff> existingStaff = staffRepository.findById(id);
+
+        if (existingStaff.isPresent()) {
+            Staff s = existingStaff.get();
+            s.setName(staff.getName());
+            s.setRole(staff.getRole());
+            s.setPhone(staff.getPhone());
+            s.setEmail(staff.getEmail());
+            s.setSalary(staff.getSalary());
+            return staffRepository.save(s);
+        } else {
+            return null;
+        }
     }
 
 }
