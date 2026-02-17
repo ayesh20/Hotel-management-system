307ccfdb4bc5dd51d793eb10191f27ed333db87d
Tharinda Pathirana <tharindapathirana27@gmail.com>
2026-02-17 19:52:33 +0530

    Implement controller and repository

--- backend/PaymentService/src/main/java/com/nsbm/group_04/Payment/PaymentApplication.java
+++ backend/PaymentService/src/main/java/com/nsbm/group_04/Payment/PaymentApplication.java
@@ -3,7 +3,7 @@ package com.nsbm.group_04.Payment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.nsbm.group_04")
 public class PaymentApplication {
 
 	public static void main(String[] args) {
