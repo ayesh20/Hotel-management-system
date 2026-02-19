2750cf4a6edf3657683141410524ea9185335335
ruvindya28 <ruvindyasachini2000@gmail.com>
2026-02-20 00:05:09 +0530

    Create add transport function

--- backend/BookingTransportService/src/main/java/com/nsbm/group_04/BookingTransportService/service/TransportService.java
+++ backend/BookingTransportService/src/main/java/com/nsbm/group_04/BookingTransportService/service/TransportService.java
@@ -1,4 +1,23 @@
 package com.nsbm.group_04.BookingTransportService.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.nsbm.group_04.BookingTransportService.entity.Transport;
+import com.nsbm.group_04.BookingTransportService.repository.TransportRepository;
+
+@Service
 public class TransportService {
+
+    @Autowired
+    private TransportRepository transportRepository;
+
+    // Register vehicle
+    public Transport addTransport(Transport transport) {
+        transport.setStatus("AVAILABLE");
+        return transportRepository.save(transport);
+    }
+
 }
