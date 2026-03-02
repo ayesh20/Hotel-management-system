7495d9ce022ad68859bb7008873663a113626cdd
yasindu2000 <yasindudahanayake@gmail.com>
2026-03-02 08:56:00 +0530

    develop impl and dto files

---
+++ backend/HousekeepingService/src/main/java/com/nsbm/group_04/Housekeeping/DTO/RoomDTO.java
@@ -0,0 +1,13 @@
+package com.nsbm.group_04.Housekeeping.DTO;
+
+public class RoomDTO {
+
+    private String id;
+    private String roomNumber;
+
+    public String getId() { return id; }
+    public void setId(String id) { this.id = id; }
+
+    public String getRoomNumber() { return roomNumber; }
+    public void setRoomNumber(String roomNumber) { this.roomNumber = roomNumber; }
+}
