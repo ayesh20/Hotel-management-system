8cb87549a3894490fdffab98ca81b3d6f7fd50c1
ruvindya28 <ruvindyasachini2000@gmail.com>
2026-03-04 23:51:14 +0530

    Create dto files for transport booking

--- backend/BookingTransportService/src/main/java/com/nsbm/group_04/BookingTransportService/dto/TransportBookingRequestDTO.java
+++ backend/BookingTransportService/src/main/java/com/nsbm/group_04/BookingTransportService/dto/TransportBookingRequestDTO.java
@@ -1,4 +1,23 @@
 package com.nsbm.group_04.BookingTransportService.dto;
 
 public class TransportBookingRequestDTO {
+
+    private String customerId;
+    private String vehicleId;
+    private String fromLocation;
+    private String toLocation;
+
+    public TransportBookingRequestDTO() {}
+
+    public String getCustomerId() { return customerId; }
+    public void setCustomerId(String customerId) { this.customerId = customerId; }
+
+    public String getVehicleId() { return vehicleId; }
+    public void setVehicleId(String vehicleId) { this.vehicleId = vehicleId; }
+
+    public String getFromLocation() { return fromLocation; }
+    public void setFromLocation(String fromLocation) { this.fromLocation = fromLocation; }
+
+    public String getToLocation() { return toLocation; }
+    public void setToLocation(String toLocation) { this.toLocation = toLocation; }
 }
