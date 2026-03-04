186702d27896dcb54b70a5a163d3fbfe0b3b3c33
ruvindya28 <ruvindyasachini2000@gmail.com>
2026-03-05 00:59:27 +0530

    add customer integration via RestTemplate

--- backend/BookingTransportService/src/main/java/com/nsbm/group_04/BookingTransportService/controller/TransportBookingController.java
+++ backend/BookingTransportService/src/main/java/com/nsbm/group_04/BookingTransportService/controller/TransportBookingController.java
@@ -1,4 +1,25 @@
 package com.nsbm.group_04.BookingTransportService.controller;
 
+import com.nsbm.group_04.BookingTransportService.dto.*;
+import com.nsbm.group_04.BookingTransportService.entity.Transport;
+import com.nsbm.group_04.BookingTransportService.service.TransportBookingService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/transport-bookings")
+@CrossOrigin(origins = "http://localhost:5173")
 public class TransportBookingController {
+
+    @Autowired
+    private TransportBookingService service;
+
+    @GetMapping("/customers")
+    public List<CustomerDTO> getCustomers() {
+        return service.getCustomers();
+    }
+
 }
--- backend/BookingTransportService/src/main/java/com/nsbm/group_04/BookingTransportService/service/TransportBookingService.java
+++ backend/BookingTransportService/src/main/java/com/nsbm/group_04/BookingTransportService/service/TransportBookingService.java
@@ -1,4 +1,44 @@
 package com.nsbm.group_04.BookingTransportService.service;
 
+import com.nsbm.group_04.BookingTransportService.dto.*;
+import com.nsbm.group_04.BookingTransportService.entity.Transport;
+import com.nsbm.group_04.BookingTransportService.entity.TransportBooking;
+import com.nsbm.group_04.BookingTransportService.repository.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
+
+@Service
 public class TransportBookingService {
+
+    @Autowired
+    private TransportBookingRepository bookingRepository;
+
+    @Autowired
+    private TransportRepository transportRepository;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    private final String CUSTOMER_SERVICE_URL =
+            "http://13.229.46.111:8080/customers";
+
+    //  Get Customers
+    public List<CustomerDTO> getCustomers() {
+
+        CustomerDTO[] customers =
+                restTemplate.getForObject(
+                        CUSTOMER_SERVICE_URL,
+                        CustomerDTO[].class
+                );
+
+        return Arrays.asList(customers);
+    }
+
+
 }
